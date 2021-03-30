20f572a88f4070a1f8d162f2ece9283adaa8ff30
Jacob Hampton <jhampton792@gmail.com>
2021-03-30 14:35:25 -0500

    Cleaned up some of the commented secitons in Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,29 +1,14 @@
 import React, { useState } from 'react'
 
 import FlightsContainer from './FlightsContainer'
-import SearchForm from '../components/SearchForm'
+// import SearchForm from '../components/SearchForm'
 import FlightSearch from '../components/FlightSearch'
 
 const Main = ({ currentUser }) => {
 
     // const [renderFlights, setRenderFlights] = useState(false)
-    const [searchRequest, setSearchRequest] = useState({})
+    // const [searchRequest, setSearchRequest] = useState({})
     const [flightsList, setFlightsList] = useState(null)
-
-    // const passSearch = (dateStart, dateEnd, locStart, locEnd) => {
-    //     // setRenderFlights(false)
-    //     setSearchRequest({startDate: dateStart, endDate: dateEnd, depart: locStart, arrive: locEnd})
-    //     // setRenderFlights(true)
-    // }
-
-    // const searchObj = (e) => {
-    //     return {
-    //         startDate: departDate,
-    //         endDate: returnDate,
-    //         locStart: targ.depart_location.value,
-    //         locEnd: targ.destination.value
-    //     }
-    // }
 
     const searchFlights = (obj, updateFlights) => {
         const reqObj = {
@@ -44,41 +29,6 @@ const Main = ({ currentUser }) => {
             })
         // setSearchRequest(obj)
     }
-
-    // searchFlights = () => {
-    //     const token = localStorage.getItem('token')
-    //         fetch("http://localhost:3000/api/v1/search_flights",{
-    //             method: "POST",
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`,
-    //                 "Content-Type": "application/json",
-    //                 "Accept": "application/json"
-    //             }, 
-    //             body: JSON.stringify({searchTerm: this.props.searchRequest})
-    //         })
-    //             .then( resp => resp.json() )
-    //             .then( resp => this.setState( { flights: resp.data } ) )
-    //     this.setState({searchRequest: this.props.searchRequest})
-    // }
-
-    // const searchFlights = () => {
-    //     const reqObj = {
-    //         method: "POST",
-    //         headers: {
-    //             Authorization: `Bearer ${localStorage.getItem('token')}`,
-    //             "Content-Type": "application/json",
-    //             Accept: "application/json"
-    //         },
-    //         body: JSON.stringify({searchTerm: searchRequest})
-    //     }
-
-    //     fetch('http://localhost:3000/api/v1/search_flights', reqObj)
-    //         .then(res => res.json())
-    //         .then(resData => {
-    //             setFlightsList(resData.data)
-    //         })
-    //     setSearchRequest(searchRequest)
-    // }
 
     return (
         <div>
