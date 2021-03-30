2b343643df1269701db76052b42faa31a8769a5c
Jacob Hampton <jhampton792@gmail.com>
2021-03-30 14:33:16 -0500

    Flight cards render current flights list after new searches

--- src/containers/FlightsContainer.js
+++ src/containers/FlightsContainer.js
@@ -1,15 +1,8 @@
 import React from 'react'
 
-// import Flight from '../components/Flight'
 import FlightCard from '../components/FlightCard'
-// import DestinationTitleBar from '../components/DestinationTitleBar'
 
 export default class Flights extends React.Component {
-    state = {
-         flights: []
-    //     // dictionary:[],
-    //     // searchRequest:[]
-    }
 
     componentDidMount() {
         //server fetch
@@ -20,22 +13,6 @@ export default class Flights extends React.Component {
         // fetch("http://localhost:3000/dictionaries").then(res=>res.json()).then(dictionary => this.setState({dictionary: dictionary}))
         
     }
-
-    // searchFlights = () => {
-    //     const token = localStorage.getItem('token')
-    //         fetch("http://localhost:3000/api/v1/search_flights",{
-    //             method: "POST",
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`,
-    //                 "Content-Type": "application/json",
-    //                 "Accept": "application/json"
-    //             }, 
-    //             body: JSON.stringify({searchTerm: this.props.searchRequest})
-    //         })
-    //             .then( resp => resp.json() )
-    //             .then( resp => this.setState( { flights: resp.data } ) )
-    //     this.setState({searchRequest: this.props.searchRequest})
-    // }
     
     render() {
         return (
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -10,10 +10,39 @@ const Main = ({ currentUser }) => {
     const [searchRequest, setSearchRequest] = useState({})
     const [flightsList, setFlightsList] = useState(null)
 
-    const passSearch = (dateStart, dateEnd, locStart, locEnd) => {
-        // setRenderFlights(false)
-        setSearchRequest({startDate: dateStart, endDate: dateEnd, depart: locStart, arrive: locEnd})
-        // setRenderFlights(true)
+    // const passSearch = (dateStart, dateEnd, locStart, locEnd) => {
+    //     // setRenderFlights(false)
+    //     setSearchRequest({startDate: dateStart, endDate: dateEnd, depart: locStart, arrive: locEnd})
+    //     // setRenderFlights(true)
+    // }
+
+    // const searchObj = (e) => {
+    //     return {
+    //         startDate: departDate,
+    //         endDate: returnDate,
+    //         locStart: targ.depart_location.value,
+    //         locEnd: targ.destination.value
+    //     }
+    // }
+
+    const searchFlights = (obj, updateFlights) => {
+        const reqObj = {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({searchTerm: obj})
+        }
+
+        fetch('http://localhost:3000/api/v1/search_flights', reqObj)
+            .then(res => res.json())
+            .then(resData => {
+                console.log(resData)
+                updateFlights(resData.data)
+            })
+        // setSearchRequest(obj)
     }
 
     // searchFlights = () => {
@@ -32,24 +61,24 @@ const Main = ({ currentUser }) => {
     //     this.setState({searchRequest: this.props.searchRequest})
     // }
 
-    const searchFlights = () => {
-        const reqObj = {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: JSON.stringify({searchTerm: searchRequest})
-        }
+    // const searchFlights = () => {
+    //     const reqObj = {
+    //         method: "POST",
+    //         headers: {
+    //             Authorization: `Bearer ${localStorage.getItem('token')}`,
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json"
+    //         },
+    //         body: JSON.stringify({searchTerm: searchRequest})
+    //     }
 
-        fetch('http://localhost:3000/api/v1/search_flights', reqObj)
-            .then(res => res.json())
-            .then(resData => {
-                setFlightsList(resData.data)
-            })
-        setSearchRequest(searchRequest)
-    }
+    //     fetch('http://localhost:3000/api/v1/search_flights', reqObj)
+    //         .then(res => res.json())
+    //         .then(resData => {
+    //             setFlightsList(resData.data)
+    //         })
+    //     setSearchRequest(searchRequest)
+    // }
 
     return (
         <div>
@@ -58,7 +87,8 @@ const Main = ({ currentUser }) => {
                 <div className="row">
                     <div className="col-md-6 prof-column">
                         {/* <SearchForm passSearch={passSearch} /> */}
-                        <FlightSearch passSearch={passSearch} />
+                        {/* <FlightSearch passSearch={passSearch} /> */}
+                        <FlightSearch searchFlights={searchFlights} updateFlights={setFlightsList} />
                         {flightsList ? <FlightsContainer searchFlights={searchFlights} flights={flightsList} searchRequest={searchRequest} /> : null}
                     </div>
                     <div className="col-md-3 prof-column">
