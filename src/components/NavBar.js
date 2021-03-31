9596e8bd5418691a7f8b60cf9b5719de0b001bc2
Jacob Hampton <jhampton792@gmail.com>
2021-03-31 12:04:17 -0500

    Resolved some merge issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,14 @@ const NavBar = ({ logoutAction }) => {
     return (
         <nav style={{ width: "100%", backgroundColor: "#1b2021", height: "5rem", marginBottom: "1rem", position: "fixed", top: "0", zIndex: "0" }} className="navbar">
             <div className="container">
-                <div className="col" style={{ width: "80%", display: "inline-block" }}>
-                    <h3 style={{ display: "inline-block", marginLeft: ".5rem", marginRight: ".5rem" }}>Trip Viewer</h3>
+                <div style={{ width: "90%", display: "inline-block" }}>
+                    <h2 style={{ display: "inline-block", marginLeft: ".5rem", marginRight: ".5rem" }}>Trip Viewer</h2>
                     <a className="router-option" href="/">Home</a>
                     <a className="router-option" href="/flights">Flights</a>
                     <a className="router-option" href="/trips">Trips</a>
                     <a className="router-option" href="/about">About</a>
                 </div>
-                <div className="col" style={{ width: "10%", display: "inline-block"}}>
+                <div style={{ width: "5%", display: "inline-block"}}>
                     <button onClick={() => logoutAction()} className="btn-sm btn-danger">Logout</button>
                 </div>
             </div>
